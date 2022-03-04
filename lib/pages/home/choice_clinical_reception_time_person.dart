179f878c4c856ec902ce9385ce00bbbb1e686007
闫瑞锋 <yrjwcharm>
2022-03-04 10:42:06 +0800

    修复list集合

--- lib/pages/home/choice_clinical_reception_time_person.dart
+++ lib/pages/home/choice_clinical_reception_time_person.dart
@@ -1,9 +1,12 @@
 import 'package:doctor_project/common/style/gsy_style.dart';
 import 'package:doctor_project/utils/colors_utils.dart';
+import 'package:doctor_project/utils/log_utils.dart';
+import 'package:doctor_project/utils/text_utils.dart';
 import 'package:doctor_project/utils/toast_utils.dart';
 import 'package:doctor_project/widget/custom_app_bar.dart';
 import 'package:doctor_project/widget/custom_input_widget.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class ChoiceClinicReceptTimePerson extends StatefulWidget {
@@ -16,12 +19,30 @@ class ChoiceClinicReceptTimePerson extends StatefulWidget {
 
 class _ChoiceClinicReceptTimePersonState
     extends State<ChoiceClinicReceptTimePerson> {
+  String startTime = '';
+  String endTime = '';
+  List list = [];
+
   _selStartTime() {}
+
+  @override
+  void initState() {
+    super.initState();
+    list = [
+      {'day': '周一', 'time': '12:30', 'checked': false},
+      {'day': '周二', 'time': '12:31', 'checked': true},
+      {'day': '周三', 'time': '01:01', 'checked': false},
+      {'day': '周四', 'time': '01:02', 'checked': false},
+      {'day': '周五', 'time': '01:03', 'checked': false},
+      {'day': '周六', 'time': '01:04', 'checked': false},
+      {'day': '周日', 'time': '01:05', 'checked': false}
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ColorsUtil.bgColor,
+      backgroundColor: Colors.white,
       body: Column(
         children: [
           CustomAppBar(
@@ -44,10 +65,10 @@ class _ChoiceClinicReceptTimePersonState
                       color: '#333333'),
                 ),
                 GestureDetector(
-                  onTap: (){
+                  onTap: () {
                     ToastUtil.showToast(msg: '点击了');
                   },
-                  child:Image.asset(
+                  child: Image.asset(
                     'assets/images/home/add.png',
                   ),
                 )
@@ -55,90 +76,158 @@ class _ChoiceClinicReceptTimePersonState
             ),
           ),
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                children: [
-                  SizedBox(
-                    width: 66,
-                    height: 56,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          primary: ColorsUtil.hexStringColor('#F9F9F9'),
-                          shape: const RoundedRectangleBorder(
-                              borderRadius: BorderRadius.only(
-                                  topRight: Radius.circular(5.0)))),
-                      onPressed: () {},
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
+              Column(
+                  children: list
+                      .asMap()
+                      .keys
+                      .map((index) => Container(
+                            margin: const EdgeInsets.only(right: 32.0),
+                            width: 66,
+                            height: 56,
+                            child: Stack(
+                              children: [
+                                ElevatedButton(
+                                  style: list[index]['checked']
+                                      ? ElevatedButton.styleFrom(
+                                          primary: Colors.white,
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.only(
+                                                  topRight: Radius.circular(
+                                                      (index == 0 ? 5.0 : 0)))))
+                                      : ElevatedButton.styleFrom(
+                                          primary: ColorsUtil.hexStringColor(
+                                              '#F9F9F9'),
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.only(
+                                                  topRight: Radius.circular(
+                                                      index == 0 ? 5.0 : 0)))),
+                                  onPressed: () {
+                                    list.forEach((item) => {
+                                      item['checked']=false,
+                                      if (mapEquals(item, list[index]))
+                                        item['checked'] = true,
+                                    });
+                                    setState(() {
+
+                                    });
+                                  },
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                      Text(
+                                        list[index]['day'],
+                                        style: GSYConstant.textStyle(
+                                            color: list[index]['checked']
+                                                ? '#06B48D'
+                                                : '#333333'),
+                                      ),
+                                      Text(
+                                        list[index]['time'],
+                                        style: GSYConstant.textStyle(
+                                            color: list[index]['checked']
+                                                ? '#06B48D'
+                                                : '#333333',
+                                            fontSize: 12.0),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                list[index]['checked']
+                                    ? Positioned(
+                                        top: 13,
+                                        child: Container(
+                                          width: 3,
+                                          height: 30,
+                                          decoration: BoxDecoration(
+                                              color: ColorsUtil.primaryColor),
+                                        ))
+                                    : Container()
+                              ],
+                            ),
+                          ))
+                      .toList()),
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      width: 176,
+                      height: 37,
+                      decoration: BoxDecoration(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(5.0)),
+                          border: Border.all(
+                              color: ColorsUtil.hexStringColor('#cccccc',
+                                  alpha: 0.3))),
+                      child: Row(
                         children: <Widget>[
-                          Text(
-                            '周一',
-                            style: GSYConstant.textStyle(color: '#333333'),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                                padding: const EdgeInsets.all(0.0)),
+                            onPressed: _selStartTime,
+                            child: Text(
+                              TextUtils.isEmpty(startTime) ? '开始时间' : startTime,
+                              style: GSYConstant.textStyle(
+                                  color: TextUtils.isEmpty(startTime)
+                                      ? '#999999'
+                                      : '#333333'),
+                            ),
                           ),
                           Text(
-                            '12:30',
-                            style: GSYConstant.textStyle(
-                                color: '#333333', fontSize: 12.0),
+                            '至',
+                            style: GSYConstant.textStyle(color: '#888880'),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              TextButton(
+                                  onPressed: _selStartTime,
+                                  child: Text(
+                                    TextUtils.isEmpty(endTime)
+                                        ? '结束时间'
+                                        : endTime,
+                                    style: GSYConstant.textStyle(
+                                        color: TextUtils.isEmpty(endTime)
+                                            ? '#999999'
+                                            : '#333333'),
+                                  )),
+                              Image.asset('assets/images/home/time.png')
+                            ],
                           ),
                         ],
                       ),
                     ),
-                  )
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Container(
-                    width: 176,
-                    height: 37,
-                    decoration: BoxDecoration(
-                        borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-                        border: Border.all(color: ColorsUtil.hexStringColor('#cccccc',alpha: 0.3))
+                    const SizedBox(
+                      width: 32.0,
                     ),
-                    child: Row(
-                      children: <Widget>[
-                        TextButton(
-                          style: TextButton.styleFrom(
-                              padding: const EdgeInsets.all(0.0)
-                          ),
-                          onPressed: _selStartTime,
-                          child: Text(
-                            '开始时间',
-                            style: GSYConstant.textStyle(color: '#333333'),
-                          ),
-                        ),
-                        Text(
-                          '至',
-                          style: GSYConstant.textStyle(color: '#888880'),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            TextButton(
-                                onPressed: _selStartTime,
-                                child: Text(
-                                  '结束时间',
-                                  style: GSYConstant.textStyle(color: '#333333'),
-                                )),
-                            Image.asset('assets/images/home/time.png')
-                          ],
-                        ),
-                      ],
+                    SizedBox(
+                      height: 37,
+                      width: 71,
+                      child: CustomInputWidget(
+                        hintText: '接诊人数',
+                        hintStyle: GSYConstant.textStyle(
+                            fontSize: 13.0, color: '#999999'),
+                        left: 0,
+                        textStyle: GSYConstant.textStyle(
+                            fontSize: 13.0, color: '#333333'),
+                        onChanged: (String value) {},
+                      ),
                     ),
-                  ),
-                  SizedBox(
-                    height: 37,
-                    width: 71,
-                    child: CustomInputWidget(hintText: '接诊人数', hintStyle: GSYConstant.textStyle(
-                      fontSize: 13.0
-                    ), textStyle: GSYConstant.textStyle(fontSize: 13.0,color: '#333333'), onChanged: (String value) {
-
-                    },),
-                  )
-                ],
-              ),
+                    Expanded(
+                        child: Container(
+                      alignment: Alignment.centerRight,
+                      margin: const EdgeInsets.only(right: 16.0),
+                      child: const Image(
+                        image: AssetImage('assets/images/home/minus.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ))
+                  ],
+                ),
+              )
             ],
-          )
+          ),
         ],
       ),
     );
--- lib/widget/custom_input_widget.dart
+++ lib/widget/custom_input_widget.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 class CustomInputWidget extends StatefulWidget {
   final bool obscureText;
   final double radius;
+  final double left;
   final String? hintText;
   final TextStyle? hintStyle;
   final IconData? iconData;
@@ -14,7 +15,6 @@ class CustomInputWidget extends StatefulWidget {
   final TextStyle? textStyle;
   final int? maxLines;
   final TextEditingController? controller;
-  final double left;
   CustomInputWidget(
       {Key? key,
       required this.hintText,
@@ -22,8 +22,8 @@ class CustomInputWidget extends StatefulWidget {
       this.radius=5.0,
       required this.onChanged,
       required this.textStyle,
-      this.controller, this.left=16.0,
-      this.obscureText = false, this.maxLines, this.hintStyle})
+      this.controller,
+      this.obscureText = false, this.maxLines, this.hintStyle, this.left=16.0})
       : super(key: key);
 
   @override
@@ -37,13 +37,13 @@ class _CustomInputWidgetState extends State<CustomInputWidget> {
   Widget build(BuildContext context) {
     return  TextField(
         controller: widget.controller,
-       onChanged: widget.onChanged,
+        onChanged: widget.onChanged,
         maxLines: widget.maxLines,
         obscureText: widget.obscureText,
         decoration: InputDecoration(
             hintText: widget.hintText,
-            fillColor: Colors.red,//背景颜色，必须结合filled: true,才有效
-            filled: true,//重点，必须设置为true，fillColor才有效
+            // fillColor: Colors.red,//背景颜色，必须结合filled: true,才有效
+            // filled: true,//重点，必须设置为true，fillColor才有效
             icon: widget.iconData == null ? null :  Icon(widget.iconData),
             border:OutlineInputBorder(
               gapPadding: 0,
@@ -53,10 +53,13 @@ class _CustomInputWidgetState extends State<CustomInputWidget> {
                 color: ColorsUtil.hexStringColor('#cccccc',alpha: 0.3),
               ),
             ),
-            contentPadding: const EdgeInsets.fromLTRB(16.0, 0, 0, 0),
+            contentPadding: EdgeInsets.only(left:widget.left,right:0,top:0,bottom: 0),
             hintStyle: widget.hintStyle
         ),
-        textDirection: TextDirection.ltr,
+        // textAlign: TextAlign.center,
+        // textAlignVertical: TextAlignVertical.center,
+        textAlign: TextAlign.center,
+        // textDirection: TextDirection.rtl,
         style: widget.textStyle,//输入文本的样式
         // inputFormatters: [WhitelistingTextInputFormatter.digitsOnly],//允许的输入格式
         cursorColor: ColorsUtil.hexStringColor('#666666')
